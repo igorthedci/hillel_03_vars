591bd8adf62da1d7d7dcfdf931be3bebb6d3a3ca
igorthedci <foumart@ukr.net>
2019-09-16 20:50:52 +0300

    task_02

---
+++ src/main/java/task_02.java
@@ -0,0 +1,16 @@
+public class task_02 {
+
+    // 2. Write a program which creates and initializes all primitive data types.
+
+    public static void main(String[] args) {
+        boolean var_boolean = true;
+        char var_char = 'w';
+        byte var_byte = 98;
+        short var_short = 12;
+        int var_int = 123;
+        long var_long = 1233445678;
+        float var_float = (float) 2125643.;
+        double var_double = 232323.3423;
+
+    }
+}
