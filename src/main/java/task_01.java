ebbb688015792e2936e3e2602ff12d7560441847
igorthedci <foumart@ukr.net>
2019-09-16 20:42:02 +0300

    task_01

---
+++ src/main/java/task_01.java
@@ -0,0 +1,15 @@
+public class task_01 {
+
+    // 1. Write a program which prints to the console some string 5 times.
+
+    public static void main(String[] args) {
+        String task1 = "Tested string";
+
+        System.out.println(task1);
+        System.out.println(task1);
+        System.out.println(task1);
+        System.out.println(task1);
+        System.out.println(task1);
+
+    }
+}
