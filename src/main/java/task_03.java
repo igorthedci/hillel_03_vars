93f50ab0ce15caa655a5a5e50dce257804445b10
igorthedci <foumart@ukr.net>
2019-09-17 22:52:51 +0300

    task_03

---
+++ src/main/java/task_03.java
@@ -0,0 +1,20 @@
+public class task_03 {
+
+    /**
+     *  3. Create a program which calculates area of a rectangle.
+     *
+     * 1) Create 2 different int variables for width and length (a and b).
+     * 2) Create 3rd variable and assign the result of the multiplication of a and b.
+     * 3) Print the result to the console."
+     */
+
+    public static void main(String[] args) {
+        int a = 12; // first side of the rectangle
+        int b = 5; // second side
+        int area; // result
+
+        area = a * b;
+
+        System.out.println("The square of rectangle is " + area);
+    }
+}
