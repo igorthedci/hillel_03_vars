7929e26922da68a527c29b95b21aacf646d6a36b
igorthedci <foumart@ukr.net>
2019-09-17 23:33:33 +0300

    task_07

---
+++ src/main/java/task_07.java
@@ -0,0 +1,35 @@
+/**
+ * "7. Comment variables which are never used.
+ * public class Education {
+ *     public static void main(String[] args) {
+ *         int a = 10;
+ *         int b = 15;
+ *         double c = b + 38;
+ *         int d = a + 12;
+ *         double e = 12.3;
+ *         String s = ""s"" + a;
+ *         String s1 = a + ""b"";
+ *         String s2 = ""a"";
+ *         String s3 = s1 + ""a"";
+ *         String s4 = s3 + ""b"";
+ *         System.out.println(c + s4 + s);
+ *     }
+ * }"
+ * "1) Do not change the arguments of functions.
+ * 2) Comment the redundant lines."
+ */
+public class task_07 {
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 15;
+            double c = b + 38;
+//            int d = a + 12;
+//            double e = 12.3;
+            String s = "s" + a;
+            String s1 = a + "b";
+//            String s2 = "a";
+            String s3 = s1 + "a";
+            String s4 = s3 + "b";
+            System.out.println(c + s4 + s);
+        }
+}
