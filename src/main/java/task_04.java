da65927db9d3e13fc35674736d2b713131da180f
igorthedci <foumart@ukr.net>
2019-09-17 23:02:39 +0300

    task_04

---
+++ src/main/java/task_04.java
@@ -0,0 +1,29 @@
+    /**
+     *  "4. Fix the program to get the correct result:
+     * public class Education {
+     *     public static int a = 5;
+     *     public static int b = 10;
+     *     public static void main(String[] args) {
+     *
+     *         int result = a / b;
+     *
+     *         System.out.println(result);
+     *     }
+     * }
+     *
+     * 1) Do not change the values  of the variables.
+     * 2) The result should be 0.5;
+     */
+
+    class Education {
+        public static int a = 5;
+        public static int b = 10;
+
+        public static void main(String[] args) {
+
+            float result = (float) a / b;
+
+            System.out.println(result);
+        }
+    }
+
